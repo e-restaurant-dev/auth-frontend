9f1c9b12aff2af3bdb9911b41e43de8cbb3c8e60
Maxim Iakovlev <max.yakovlev555@gmail.com>
2022-08-16 15:57:53 +0200

    working variant

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <div>123</div>
-  </React.StrictMode>
+    <React.StrictMode>
+        <div>123</div>
+    </React.StrictMode>,
 )
